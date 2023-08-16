af1892d173d899d24e52300ac619be4e22b44578
lechihieu99 <lechihieu99us@gmail.com>
2023-08-16 15:51:21 +0700

    change audio

--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -90,8 +90,8 @@ const Player = ({ isOpen, setIsOpen, isPlaying, setIsPlaying, playingList, setPl
     }, [playingList, favouriteList])
 
     useEffect(() => {
-        console.log(isOpen)
-    }, [isOpen])
+        console.log(selectedSong)
+    }, [selectedSong])
 
     useEffect(() => {
         /**
@@ -167,7 +167,7 @@ const Player = ({ isOpen, setIsOpen, isPlaying, setIsPlaying, playingList, setPl
         <>
             {token && selectedId && (
                 <>
-                    <audio className="hidden" id="myAudio" controls src={`${selectedSong}`} onEnded={isRepeatOnce ? repeatAudio : handleNextSong} >
+                    <audio className="" id="myAudio" controls src={`${selectedSong}`} onEnded={isRepeatOnce ? repeatAudio : handleNextSong} >
                     </audio>
                     {hover && (
                         <div className={`hidden sm:block w-full h-56 rounded-md bg-black p-2 overflow-y-auto`} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
