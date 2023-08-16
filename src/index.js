94c16e6ff2f64348701539ada4f9b719e9532538
lechihieu99 <lechihieu99us@gmail.com>
2023-08-16 10:15:43 +0700

    hash router with github pages

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ import reportWebVitals from './reportWebVitals';
 
 import { store } from './redux/app/store';
 import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <HashRouter>
+        <App />
+
+      </HashRouter>
     </Provider>
 
   </React.StrictMode>
