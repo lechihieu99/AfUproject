e8005174dbbfafc92edb7ac39604899d2ffb92a8
lechihieu99 <lechihieu99us@gmail.com>
2023-11-09 08:10:37 +0700

    change url

--- src/constants/Pathname.js
+++ src/constants/Pathname.js
@@ -1,3 +1,3 @@
-export const API_HOST = 'https://63eb-27-74-206-54.ngrok-free.app/api';
-export const HOST = 'https://63eb-27-74-206-54.ngrok-free.app/'
+export const API_HOST = 'https://4d44-14-161-14-172.ngrok-free.app/api';
+export const HOST = 'https://4d44-14-161-14-172.ngrok-free.app/'
 // export const API_HOST = 'http://10.0.249.109:3114/api';
