0f306a523272bc12aa6a6ac78cfe07614dab9c9c
lechihieu99 <lechihieu99us@gmail.com>
2023-12-19 09:26:03 +0700

    update server

--- src/constants/Pathname.js
+++ src/constants/Pathname.js
@@ -1,3 +1,3 @@
-export const API_HOST = 'https://57f5-2402-800-f59a-7bc9-44c9-afc5-e74-7d05.ngrok-free.app/api';
-export const HOST = 'https://57f5-2402-800-f59a-7bc9-44c9-afc5-e74-7d05.ngrok-free.app/'
+export const API_HOST = 'https://2cc9-2402-800-f59a-7bc9-44c9-afc5-e74-7d05.ngrok-free.app/api';
+export const HOST = 'https://2cc9-2402-800-f59a-7bc9-44c9-afc5-e74-7d05.ngrok-free.app/'
 // export const API_HOST = 'http://10.0.249.109:3114/api';
