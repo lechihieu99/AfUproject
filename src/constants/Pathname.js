f1e27c064c429d9038a299d7de0914b0afe13d16
lechihieu99 <lechihieu99us@gmail.com>
2023-12-20 08:11:29 +0700

    fix url server

--- src/constants/Pathname.js
+++ src/constants/Pathname.js
@@ -1,3 +1,3 @@
-export const API_HOST = 'https://2cc9-2402-800-f59a-7bc9-44c9-afc5-e74-7d05.ngrok-free.app/api';
-export const HOST = 'https://2cc9-2402-800-f59a-7bc9-44c9-afc5-e74-7d05.ngrok-free.app/'
+export const API_HOST = 'https://76d2-2402-800-5ca8-6128-5079-5ff4-4988-791.ngrok-free.app/api';
+export const HOST = 'https://76d2-2402-800-5ca8-6128-5079-5ff4-4988-791.ngrok-free.app/'
 // export const API_HOST = 'http://10.0.249.109:3114/api';
